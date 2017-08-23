3c8e5e2120e60642e25641212317a19d63fee51f
BoykoAlex <aboyko@pivotal.io>
2017-08-23 15:08:08 -0400

    Remove built *.d.ts files from the repo

--- src/demo/app/graph-to-text.d.ts
+++
@@ -1,2 +0,0 @@
-import { dia } from 'jointjs';
-export declare function convertGraphToText(g: dia.Graph): string;
--- src/demo/app/text-to-graph.d.ts
+++
@@ -1,2 +0,0 @@
-import { Flo } from 'spring-flo';
-export declare function convertTextToGraph(flo: Flo.EditorContext, metamodel: Map<string, Map<string, Flo.ElementMetadata>>, input: string): void;
