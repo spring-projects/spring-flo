6c25da8783e11da90f48785b89ed35bf79eb9c80
BoykoAlex <aboyko@pivotal.io>
2017-08-30 17:19:45 -0400

    Export Shapes namespace

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,10 +2,10 @@ export { FloModule } from './src/module';
 export { Palette } from './src/palette/palette.component';
 export { EditorComponent } from './src/editor/editor.component';
 export { DslEditorComponent } from './src/dsl-editor/dsl.editor.component';
-export { Constants } from './src/shared/shapes';
 export { PropertiesGroupComponent } from './src/properties/properties.group.component';
 export { DynamicFormPropertyComponent } from './src/properties/df.property.component';
 export { ResizerDirective } from './src/directives/resizer';
 
 export * from './src/shared/flo.common';
 export * from './src/shared/flo.properties';
+export * from './src/shared/shapes';
