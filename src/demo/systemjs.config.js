56c73a899181898ab6244b3e894f06093d1fd954
BoykoAlex <aboyko@pivotal.io>
2019-06-06 17:25:57 -0400

    Upgrade ngx-bootstrap for sample app

--- src/demo/systemjs.config.js
+++ src/demo/systemjs.config.js
@@ -23,12 +23,36 @@
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+      '@angular/animations': 'npm:@angular/animations/bundles/animations.umd.js',
+
       'ngx-bootstrap': 'npm:ngx-bootstrap/bundles/ngx-bootstrap.umd.min.js',
+      // Below workaround due to https://github.com/valor-software/ngx-bootstrap/issues/5195
+      'ngx-bootstrap/chronos': 'npm:ngx-bootstrap/chronos/bundles/ngx-bootstrap-chronos.umd.min.js',
+      'ngx-bootstrap/utils': 'npm:ngx-bootstrap/utils/bundles/ngx-bootstrap-utils.umd.min.js',
+      'ngx-bootstrap/dropdown': 'npm:ngx-bootstrap/dropdown/bundles/ngx-bootstrap-dropdown.umd.min.js',
+      'ngx-bootstrap/accordion': 'npm:ngx-bootstrap/accordion/bundles/ngx-bootstrap-accordion.umd.min.js',
+      'ngx-bootstrap/collapse': 'npm:ngx-bootstrap/collapse/bundles/ngx-bootstrap-collapse.umd.min.js',
+      'ngx-bootstrap/alert': 'npm:ngx-bootstrap/alert/bundles/ngx-bootstrap-alert.umd.min.js',
+      'ngx-bootstrap/buttons': 'npm:ngx-bootstrap/buttons/bundles/ngx-bootstrap-buttons.umd.min.js',
+      'ngx-bootstrap/carousel': 'npm:ngx-bootstrap/carousel/bundles/ngx-bootstrap-carousel.umd.min.js',
+      'ngx-bootstrap/mini-ngrx': 'npm:ngx-bootstrap/mini-ngrx/bundles/ngx-bootstrap-mini-ngrx.umd.min.js',
+      'ngx-bootstrap/component-loader': 'npm:ngx-bootstrap/component-loader/bundles/ngx-bootstrap-component-loader.umd.min.js',
+      'ngx-bootstrap/positioning': 'npm:ngx-bootstrap/positioning/bundles/ngx-bootstrap-positioning.umd.min.js',
+      'ngx-bootstrap/pagination': 'npm:ngx-bootstrap/pagination/bundles/ngx-bootstrap-pagination.umd.min.js',
+      'ngx-bootstrap/progressbar': 'npm:ngx-bootstrap/progressbar/bundles/ngx-bootstrap-progressbar.umd.min.js',
+      'ngx-bootstrap/rating': 'npm:ngx-bootstrap/rating/bundles/ngx-bootstrap-rating.umd.min.js',
+      'ngx-bootstrap/tabs': 'npm:ngx-bootstrap/tabs/bundles/ngx-bootstrap-tabs.umd.min.js',
+      'ngx-bootstrap/timepicker': 'npm:ngx-bootstrap/timepicker/bundles/ngx-bootstrap-timepicker.umd.min.js',
+      'ngx-bootstrap/tooltip': 'npm:ngx-bootstrap/tooltip/bundles/ngx-bootstrap-tooltip.umd.min.js',
+      'ngx-bootstrap/typeahead': 'npm:ngx-bootstrap/typeahead/bundles/ngx-bootstrap-typeahead.umd.min.js',
+      'ngx-bootstrap/popover': 'npm:ngx-bootstrap/popover/bundles/ngx-bootstrap-popover.umd.min.js',
+      'ngx-bootstrap/locale': 'npm:ngx-bootstrap/locale/bundles/ngx-bootstrap-locale.umd.min.js',
+      'ngx-bootstrap/sortable': 'npm:ngx-bootstrap/sortable/bundles/ngx-bootstrap-sortable.umd.min.js',
+      'ngx-bootstrap/datepicker': 'npm:ngx-bootstrap/datepicker/bundles/ngx-bootstrap-datepicker.umd.min.js',
+      'ngx-bootstrap/modal': 'npm:ngx-bootstrap/modal/bundles/ngx-bootstrap-modal.umd.min.js',
 
       // other libraries
       'rxjs': 'npm:rxjs',
-      // ngx-bootstrap 2.x + rxjs-compat due to https://github.com/valor-software/ngx-bootstrap/issues/4494
-      'rxjs-compat': 'npm:rxjs-compat',
       'jointjs': 'npm:jointjs',
       'jquery': 'npm:jquery',
       'backbone': 'npm:backbone',
@@ -68,13 +92,6 @@
         main: 'index.js',
         defaultExtension: 'js'
       },
-      'rxjs-compat': {
-        main: 'index.js',
-        defaultExtension: 'js'
-      },
-      'rxjs/internal-compatibility': {
-        main: './index.js'
-      },
       'rxjs/operators': {
         main: './index.js'
       },
@@ -86,13 +103,13 @@
         main: './dist/joint.js'
       },
       jquery: {
-        main: './dist/jquery.js',
+        main: './dist/jquery.js'
       },
       backbone: {
-        main: './backbone.js',
+        main: './backbone.js'
       },
       lodash: {
-        main: './index.js',
+        main: './index.js'
       },
       dagre: {
         main: './dist/dagre.js'
